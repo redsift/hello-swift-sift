527bf389c24a1df685d3067cb3bdb25faf1ac1a9
Christos Vontas <christos@redsift.io>
2017-08-29 16:46:48 +0100

    cleanup prints

--- server/Node1.swift
+++ server/Node1.swift
@@ -6,18 +6,16 @@ public class Node1: Redsift.RedsiftNode{
   public static func compute(req: ComputeRequest) -> Any?{
     setbuf(stdout, nil)
     let inData = req.in
-    print("hello-swift-sift: Node1.swift: data received:", inData.data)
-    let ds = inData.data
-    for dq in ds{
+    print("Hello-Swift-Sift: Node1.swift: data received:", inData.data)
+    for dq in inData.data{
       // print("This is what I get \(dq.value!)")
       guard let b = dq.value,
         let jsonString = String(data: b, encoding: .utf8) else{
         print("Error: Could not decode request")
         return nil
       }
-      print("This is the string \(jsonString)")
       let a: Jmap.Message = Jmap.Message(JSONString: jsonString)!
-      print("and this is the message \(String(describing: a))")
+      print("This is the message \(String(describing: a))")
     }
     return nil
   }
